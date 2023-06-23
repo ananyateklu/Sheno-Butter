859b66cfaed7e81afc413d1fffa4f484a632b69e
Fiseha Abebe <famarkfish79@gmail.com>
2023-06-23 09:01:22 +0300

    add grid design and logo to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import "./Home.css"
 
 
 const Home:React.FC= () => {
     return(
 
-        <div>
-            Home
+        <div className="main-container">
+         <div className="Home-Navbar">
+            
+          <a href="/" className="Home-link">
+            HOME
+          </a>
+          <a href="/origins" className="Origins-link">
+            ORIGINS
+          </a>
+          
+          <a href="/aboutUs" className="About-link">
+            ABOUT US
+          </a>
+          <a href="/contactUs" className="Contact-link">
+            CONTACT US
+          </a>
+        
+        </div>
+      <div className="Home-main">
+      Main Componenets
+      </div>
         </div>
 
     );
