d155ae7d0a8ba1634d61c473010b1dbf4630511b
Fiseha Abebe <famarkfish79@gmail.com>
2023-06-23 10:27:34 +0300

    grid layouts

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,9 +6,10 @@ const Home:React.FC= () => {
     return(
 
         <div className="main-container">
+        <div className="side-bar">
          <div className="Home-Navbar">
             
-          <a href="/" className="Home-link">
+          <a href="/" className="Home-link ">
             HOME
           </a>
           <a href="/origins" className="Origins-link">
@@ -23,8 +24,32 @@ const Home:React.FC= () => {
           </a>
         
         </div>
+        <div className="sidebar-img">
+
+        </div>
+        </div>
       <div className="Home-main">
-      Main Componenets
+     
+      <div className="main-img-ctn">
+        <div className="main-img">
+mainimg
+        </div>
+        <div className="side-img1">
+          1
+        </div>
+        <div className="side-img2">
+          2
+        </div>
+        <div className="side-img3">
+          3
+        </div>
+        <div className="side-img4">
+          4
+        </div>
+      </div>
+      <div className="product-holder">
+555
+      </div>
       </div>
         </div>
 
