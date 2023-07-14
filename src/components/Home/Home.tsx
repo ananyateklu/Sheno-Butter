bfadb6639d552f401fc69063b268fab16856c8c8
Ananya Teklu <teklu009@morris.umn.edu>
2023-07-14 10:23:42 -0500

    Add small styling changes to home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -37,14 +37,20 @@ const Home: React.FC = () => {
 
   return (
     <div className="main-container">
-      <img src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-cart-supermarket-flatart-icons-flat-flatarticons.png" alt="external-cart-supermarket-flatart-icons-flat-flatarticons"
-        className="cart-button"
-        onClick={(e) => {
+      <div className="cart-bar">
+        <img src="https://img.icons8.com/external-flatart-icons-flat-flatarticons/64/external-cart-supermarket-flatart-icons-flat-flatarticons.png" alt="external-cart-supermarket-flatart-icons-flat-flatarticons"
+          className="cart-button oneb"
+          onClick={(e) => {
+            e.stopPropagation(); // Prevent click from bubbling to container
+            setIsCartVisible(!isCartVisible);
+          }}
+        /> <p className="cart-amount oneb" onClick={(e) => {
           e.stopPropagation(); // Prevent click from bubbling to container
           setIsCartVisible(!isCartVisible);
-        }}
-      /> <p className="cart-amount">{itemCount}</p>
-      {isCartVisible && <Cart isVisible={isCartVisible} onVisibilityChange={handleVisibilityChange} />}
+        }}>{itemCount}</p>
+        {isCartVisible && <Cart isVisible={isCartVisible} onVisibilityChange={handleVisibilityChange} />}
+      </div>
+
       <div className="side-bar">
         <div className="Home-Navbar">
           <a href="/origins" className="Origins-link">
